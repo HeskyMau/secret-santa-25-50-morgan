92d2c6c585166ba9517c9378a8a2f31a252c62f3
DrDriia <dd.chemloul@gmail.com>
2025-12-20 14:04:48 +0100

    Add the first memories

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const contentInfo = document.getElementById('content');
 const closeBtn = document.getElementById('close-btn');
 
 const contents = [
-    "Oouh, le cristal magique...",
-    "Coucou",
-    "Teeest",
-    "Bichou bichou, jetem Momo d'amour",
+    "Les derniers jours au travail avaient été particuliers, depuis le scandal que ton ancienne collègue avait fait. Tu l'aperçois au fond de la boutique, tenant un sac avec ses effets personnels. Elle marche lentement vers toi tel un fantôme. Lorsqu'elle t'atteint, elle s'arrête, et murmure sans même te regarder : \"Sans rancune.\" Tu sens le léger sourire dans sa voix et, en un instant, elle disparaît, comme si elle n'avait jamais été là.",
+    "Dans un univers parallèle, tu sillonnes majestueusement les cieux, baguette en main, chevauchant non pas un balai, mais un noble Noir des Hébrides - comme il se doit. Escortée d'un dragonnier-militaire, tu endosses le rôle ô combien modeste de sauveuse de l'humanité : il te faut terrasser un abominable Fléau contrôleur d'esprits. Une broutille pour l'embrumée badass que tu es, en somme.",
+    "Dans une autre vie, beaucoup plus jeune, tu jettes un souafle à la tête d'un jeune homme aux cheveux noirs. Puis tu te vois poursuivre ton aventure avec lui, tous deux perchés sur des balais, dans un stade avec une foule en fureur.",
+    "Ooouh, le cristal magique...",
     "Horglup",
     "PHOTOBOMB",
-    "blblblbl <333333",
-    "Noyeux joël",
+    "Dans la brume anglaise d'un matin d'hiver, tu peux sentir quelques flocons tomber sur tes cheveux. Lorsque tu tentes de les retirer, tu t'aperçois que la quantité s'accentue. Devant toi, un livre apparaît sous les flocons qui disparaissent. Tu peux y lire des inscriptions runiques d'un auteur allemand et un petit mot dans le coin de la première page : \"Peut-être que celui-ci, tu ne l'as pas encore lu\".",
+    "Une fois, tu as envoyé une longue lettre dans laquelle tu as mis toute ta créativité. C'était le 3 avril 2023, lorsque tu as fait part de ton souhait de donner vie à l'Allée des Embrumes. A ce moment-là, cette jeune blonde ne pensait pas avoir fait la rencontre d'une commerçante bienveillante, pleine d'idées et à qui, presque deux ans plus tard, elle passerait le flambeau en lui faisant entièrement confiance.",
     "42",
     "Horglup, mais baveux cette fois-ci"
 ]
